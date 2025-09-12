8e6a9a08174116470b2518a33de90f15b42bfa2e
David G. <armeroitop@gmail.com>
2025-09-12 21:07:44 +0200

    Añadidos parametros de area de trabajo

--- src/include/cargaParametros.hpp
+++ src/include/cargaParametros.hpp
@@ -14,6 +14,8 @@ struct Parametros {
     int velocidadMax;
     int aceleracion;
     std::string puertoSerie;
+    int ancho_mm;
+    int alto_mm;
 };
 
 Parametros cargarParametros(const std::string& ruta) {
@@ -29,6 +31,9 @@ Parametros cargarParametros(const std::string& ruta) {
     parametro.velocidadMax = j.value("velocidadMax", 100);  // valor por defecto si no existe
     parametro.aceleracion  = j.value("aceleracion", 100);
     parametro.puertoSerie  = j.value("puertoSerie", "/dev/ttyUSB0");
+
+    parametros.ancho_mm = j.value("ancho_mm", 200); // valor por defecto si no existe
+    parametros.alto_mm  = j.value("alto_mm", 200);
 
     return parametro;
 }
